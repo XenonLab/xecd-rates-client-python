952564cd1cb0d15d61622960ebeaf56199f8f8ef
Yang Lu <kuulkev@gmail.com>
2017-12-19 15:15:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,23 +6,6 @@ setup(
     version='0.1.0',
     description='XECD REST Client',
     license='MPL-2.0',
-<<<<<<< HEAD
-    author='XE',
-    url='github URL',
-    download_url='optional, can create a github hosted download tarball link',
-    keywords=[
-        'xecd',
-        'node',
-        'api',
-        'client',
-        'xe',
-        'currency',
-        'data',
-        'rest'
-    ],
-    classifiers=[]
-=======
     author='XE.com Inc. Development Team',
     description='XECD REST Client'
->>>>>>> 4c56111a84deb6ede357a6a669f02a61cce689b3
 )
